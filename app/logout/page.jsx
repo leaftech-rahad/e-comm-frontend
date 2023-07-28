86122f7fa2bdc82c55acb29df76c63a9a3adeb45
leaftech-rahad <leaftech.rahad@gmail.com>
2023-07-29 00:31:16 +0600

    solved signout issue

--- app/logout/page.jsx
+++ app/logout/page.jsx
@@ -1,22 +1,24 @@
 "use client";
-
-import React from "react";
 import Card from "../../components/Card";
 import { Button } from "../../components/Button";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { signOut } from "../../api/index";
-import { redirect } from "next/navigation";
+
 const Page = () => {
   const queryClient = useQueryClient();
-  const mutation = useMutation(signOut);
+  const mutation = useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      window.open("http://localhost:3000");
+      window.close();
+    },
+  });
+
   const handelClick = (e) => {
     e.preventDefault();
     mutation.mutate();
-    queryClient.clear();
   };
-  mutation?.data
-    ? queryClient.invalidateQueries({ queryKey: ["customer"] })
-    : null;
+
   return (
     <>
       <Card className={" w-1/2 m-auto mt-5"}>
